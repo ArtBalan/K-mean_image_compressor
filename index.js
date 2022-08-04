598ea7665623f676ac7159afe0385f9d8769e7c9
Arthur Balandreau <artbalan20@gmail.com>
2022-08-04 13:59:46 +0200

    test

--- index.js
+++ index.js
@@ -37,12 +37,11 @@ async function readImage(){
 
 function caldist(a, b) {
   // pythagore goes brrr
-  // return Math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2 + (b.z -a.z) ** 2);
   return (b.x - a.x) ** 2 + (b.y - a.y) ** 2 + (b.z -a.z) ** 2;
 }
 
 // determine the number of différent color in the export
-let nbrOfNode = 3;
+let nbrOfNode = 16;
 
 // sys var, do not toutch
 let nodeList = [];
@@ -59,25 +58,28 @@ async function main(){
 
   // Node array
   for(let i=0; i<nbrOfNode; i++){
-    let tempX = Math.floor(Math.random()*255);
-    let tempY = Math.floor(Math.random()*255);
-    let tempZ = Math.floor(Math.random()*255);
+    let tempX = Math.random()*255;
+    let tempY = Math.random()*255;
+    let tempZ = Math.random()*255;
     let tempNode = new Node(tempX,tempY,tempZ);
     nodeList.push(tempNode);
   }
 
 
-  while(!stop && step<100000000){
+  while(!stop){
     // console.log(step);
-    if(step%100000 == 0){
-      console.log(step/100000 + '%');
+
+    if(step%10000 == 0){
+      console.log('step')
+      // nodeList.forEach(node => console.log(node.pointList))
     }
+
     // Cleaning links
     nodeList.forEach(node => node.cleanPointList());
   
     // Assigns point to nearest node
     for(let i=0; i<pointList.length; i++){
-      let minDist = image.height * image.width;
+      let minDist = 1000000000;
       let minDistNodeIndex = 0;
       for(let j=0; j<nodeList.length; j++){
         let tempDist = caldist(pointList[i],nodeList[j]);
@@ -88,9 +90,8 @@ async function main(){
       }
       nodeList[minDistNodeIndex].pointList.push(i)
     }
-  
-    let stop = true;
-    let nodeFinished = 0;
+    
+    stop = true;
     nodeList.forEach(node =>{
       let tempX = 0;
       let tempY = 0;
@@ -100,32 +101,28 @@ async function main(){
         tempY += pointList[e].y;
         tempZ += pointList[e].z;
       });
-      if(node.pointList.length !=0){
+      if(node.pointList.length > 0){
         tempX /= node.pointList.length;
         tempY /= node.pointList.length;
         tempZ /= node.pointList.length;
         let tempPoint = new Point(tempX,tempY,tempZ);
-        let dist = caldist(tempPoint,node); 
-        if(dist>1){
+        if(caldist(tempPoint,node)>0.01){
+          console.log(caldist(tempPoint,node))
           stop = false;
-        } else {
-          nodeFinished ++;
         }
-        node.x = Math.floor(tempX);
-        node.y = Math.floor(tempY);
-        node.z = Math.floor(tempZ);
-        node.deltaDist = dist;
+        node.x = tempX;
+        node.y = tempY;
+        node.z = tempZ;
       }
     });
     // console.log('node potentialy finished : ' + nodeFinished);
     step ++;
   }
 
-  nodeList.forEach(node => console.log(node.x + ' ' + node.y + ' ' + node.z));
+  nodeList.forEach(node => console.log('color : rgb(' + node.x + ', ' + node.y + ', ' + node.z + ');'));
   nodeList.forEach(node => console.log('delta dist : ' + node.deltaDist));
 
   console.log('finished in ' + step + ' steps.');
-
 }
 
 main();
